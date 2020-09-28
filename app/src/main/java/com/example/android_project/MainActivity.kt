8e2ea109ea8b3d919cf088b20dbf86613fdea680
Daniel Liberato <dlj.daniel@gmail.com>
2020-09-28 13:26:39 -0300

    Loading screen - OK

--- app/src/main/java/com/example/android_project/MainActivity.kt
+++ app/src/main/java/com/example/android_project/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.android_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.android_project.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val runnable = Runnable {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        //Handler().postDelayed(runnable, 3000L)
     }
+
 }
